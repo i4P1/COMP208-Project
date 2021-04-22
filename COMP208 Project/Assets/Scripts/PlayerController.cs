7efce4eb4eeaa0c0cced04e0f0934038323436cc
RedDeadUndead <56684813+RedDeadUndead@users.noreply.github.com>
2021-04-22 12:05:33 +0100

    Part 2
    Some changes had to be stashed so I'm committing them now.

--- COMP208 Project/Assets/Scripts/PlayerController.cs
+++ COMP208 Project/Assets/Scripts/PlayerController.cs
@@ -59,6 +59,7 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     private void FixedUpdate() {
         // Stop dashing after its duration
         if((Time.time - dashStartTime) > dashDuration && dashing) {
@@ -69,6 +70,11 @@ public class PlayerController : MonoBehaviour {
         // Set the player's speed
         if (dashing) {
             rb.velocity = dashDirection.normalized * dashSpeed;
+=======
+    private void Update() {
+        if ((Time.time - hoverStartTime) < hoverDuration) {
+            rb.velocity = Vector2.zero;
+>>>>>>> Stashed changes
 =======
     private void Update() {
         if ((Time.time - hoverStartTime) < hoverDuration) {
